5d160cbbc79f6efa519b3668b24c68e1a69dc224
Couguar <ingluemlp@gmail.com>
2026-03-15 18:36:32 +0300

    Add: config celery

--- app/core/config.py
+++ app/core/config.py
@@ -21,12 +21,12 @@ class Settings(BaseSettings):
     API_VERSION: str
 
     @property
-    def REDIS_URL(self):
-        return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}"
-
-    @property
     def DB_URL(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    @property
+    def REDIS_BROKER_URL(self):
+        return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}/1"
 
     model_config = SettingsConfigDict(env_file=BASE_DIR / ".env.local", extra="ignore")
 
